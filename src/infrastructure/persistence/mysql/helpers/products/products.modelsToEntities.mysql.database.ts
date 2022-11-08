d6e169d4cf3277b1513abae90c28717b6334aaf5
jonveigel <cloudeca1@gmail.com>
2022-11-08 19:59:35 -0300

    Create helpers products

---
+++ src/infrastructure/persistence/mysql/helpers/products/products.modelsToEntities.mysql.database.ts
@@ -0,0 +1,20 @@
+//Estrutura -> helpers -> pasta da entidade -> nome deste arquivo substituindo "example"
+//Utilize esta função para retorno de Entity
+import { IProductsEntity } from "../../../../../domain/entities/products/products.entity";
+
+export default function (product: any): IProductsEntity | undefined {
+    if (!product) {
+        return;
+    }
+
+    let productOne: IProductsEntity = {
+        idProduct: product.idProduct,
+        name: product.name,
+        description: product.description,
+        image: product.image,
+        price: product.price,
+        isAvailable: product.isAvailable
+    }
+
+    return (productOne as IProductsEntity);
+}
