b231987bc1735e6ff43cde6d6ce639ffdaf54194
jonveigel <cloudeca1@gmail.com>
2022-11-08 19:19:37 -0300

    Adding entities products

---
+++ src/domain/entities/products/products.entity.ts
@@ -0,0 +1,11 @@
+//Estrutura -> entities -> pasta da entidade -> nome deste arquivo substituindo "example"
+//Interface da entidade -> sintaxe = IExampleEntity
+
+export interface IProductsEntity {
+    idProduct?: number,
+    name: string,
+    description: string,
+    image?: string,
+    price: number,
+    isAvailable: boolean  
+}
