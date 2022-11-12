687ba1ce2f85135d7bf4c53b8953569c763ccad9
Marcos Teixeira <marcxstx@gmail.com>
2022-11-12 16:20:57 -0300

    Ending first place production except

--- src/domain/usecases/tables/tables.create.usecase.ts
+++ src/domain/usecases/tables/tables.create.usecase.ts
@@ -1,17 +1,17 @@
-// import tablesRepository from "../../../adapters/repositories/tables/tables.repository";
-// import { ITablesEntity } from "../../entities/tables/tables.entity";
-// import { ITablesRepository } from "../../repositories/tables/tables.repository.interface";
-// import { IUseCase } from "../usecase.interface";
+import tablesRepository from "../../../adapters/repositories/tables/tables.repository";
+import { ITablesEntity } from "../../entities/tables/tables.entity";
+import { ITablesRepository } from "../../repositories/tables/tables.repository.interface";
+import { IUseCase } from "../usecase.interface";
 
-// class CreateTableUseCase implements IUseCase{
+class CreateTableUseCase implements IUseCase{
 
-//     constructor(private _repository: ITablesRepository){}
+    constructor(private _repository: ITablesRepository){}
 
-//     async execute(data: ITablesEntity): Promise<ITablesEntity | undefined> {
-//         return await this._repository.create(data);        
-//     }
-// }
+    async execute(data: ITablesEntity): Promise<ITablesEntity | undefined> {
+        return await this._repository.create(data);        
+    }
+}
 
-// export default new CreateTableUseCase(
-//     tablesRepository
-// );
+export default new CreateTableUseCase(
+    tablesRepository
+);
